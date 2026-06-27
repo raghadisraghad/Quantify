b593522fb67abe619625488f48683dd6a111ad02
raghadisraghad <raghad.ch.4@gmail.com>
2026-06-27 13:54:32 +0100

    gateway and discovery service working

---
+++ api_gateway_service/src/test/java/com/quantify/api_gateway_service/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantify.api_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
