492b2558444f68b599cc9e04af8874f8c72a4118
raghad chamlali <121711320+raghadisraghad@users.noreply.github.com>
2026-07-17 14:11:21 +0100

    sales service (#17)
    * sales service CRUD and basic controllers + swagger configuration
    
    * add new arguments annotations to the DTOs

--- sales-service/src/main/java/com/quantify/salesservice/DTOs/CreatePaymentRequest.java
+++ sales-service/src/main/java/com/quantify/salesservice/DTOs/CreatePaymentRequest.java
@@ -2,14 +2,18 @@ package com.quantify.salesservice.DTOs;
 
 import com.quantify.salesservice.models.PaymentMethod;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreatePaymentRequest {
 
     @NotNull
